bf9c976163d116f8e232e23f20a3aaf479b232af
mateuszbugaj <mateuszbugaj5@gmail.com>
2020-11-03 17:25:23 +0100

    initial commit

---
+++ src/main/java/FewGames/FewGamesApplication.java
@@ -0,0 +1,13 @@
+package FewGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FewGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FewGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FewGames/FewGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package FewGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FewGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
